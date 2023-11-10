51fdae282cbfd8591faf5c3c414dab37dc45cd4c
rohansinha01 <rohansinha01@gmail.com>
2023-11-09 23:02:35 -0500

    landscaper: user can use teeth to cut grass

---
+++ app.js
@@ -0,0 +1,19 @@
+const tools = [
+    {name: "Teeth",
+    costs: 0,
+    profits: 1}
+] //We need an array of the objects that define the name of the item we are using, how much we paid for it, and how much it will make us.
+
+const mower = [
+    {
+        money: 0,
+        tool: 0
+    }
+]
+
+function mowLawn(){
+    mower.money += tools.profit
+   alert("You added a dollar")
+}
+mowLawn()
+
